921be3b268ea5719c3901de5dce64d66d220e7ba
charllab <developer.charl@gmail.com>
2021-05-31 08:40:00 -0700

    added parrent tree function + back button to content template

--- src/themes/nexusbc/partials/main/content.php
+++ src/themes/nexusbc/partials/main/content.php
@@ -2,5 +2,14 @@
     <div class="pr-xl-2">
         <h1><?php the_title(); ?></h1>
         <?php the_content(); ?>
+
+
+        <?php if (!is_tree([48])) : ?>
+        <div class="py-1 mt-1">
+            <a href="javascript:history.go(-1)" class="btn btn-primary">Back</a>
+        </div>
+        <?php endif; ?>
+
+
     </div><!-- pr -->
 </div><!-- col-->
