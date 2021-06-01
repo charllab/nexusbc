028763fb700b6a432b0d75dbdc2e92506b3cfa42
charllab <developer.charl@gmail.com>
2021-06-01 11:31:16 -0700

    added Google Tag Manager hook to head

--- src/themes/nexusbc/header.php
+++ src/themes/nexusbc/header.php
@@ -10,10 +10,33 @@
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet">
+
+    <!-- Google Tag Manager -->
+    <script>(function (w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start':
+                    new Date().getTime(), event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-KLT24K7');</script>
+    <!-- End Google Tag Manager -->
+
     <script src="https://kit.fontawesome.com/61d303198f.js" crossorigin="anonymous"></script>
 
     <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-    <?php get_template_part('partials/header/hero-nav-overlay'); ?>
+<!-- Google Tag Manager (noscript) -->
+<noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KLT24K7"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
+<!-- End Google Tag Manager (noscript) -->
+<?php get_template_part('partials/header/hero-nav-overlay'); ?>
